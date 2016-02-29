4af8b26c879f0e17783a484ef40f95fc8a4c232d
BrowneShugeMoon <j7moon@ucsd.edu>
2016-02-28 21:40:47 -0800

    Added files via upload
    UPDATED 2/28

--- livingjs.js
+++ livingjs.js
@@ -5,4 +5,6 @@ $('#container').isotope({
     }
 });
 
-
+var offset = $(':target').offset();
+var scrollto = offset.top - 108; // minus fixed header height
+$('html, body').animate({scrollTop:scrollto}, 0);
